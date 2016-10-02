c47d5a6a5ed976485aad415786fd13cea50350f9
高东霞 <dongxia.gao@dazong.com>
2016-10-02 15:07:44 +0800

    更新: 简单工厂的实现Demo

---
+++ 02_SimpleFactory/Customer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_SimpleFactory
+{
+    class Customer
+    {
+        /// <summary>
+        /// 烧菜方法
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static Food Cook(string type)
+        {
+            Food food = null;
+            // 客户A说：我想吃西红柿炒蛋怎么办？
+            // 客户B说：那你就自己烧啊
+            // 客户A说： 好吧，那就自己做吧
+            if (type.Equals("西红柿炒蛋"))
+            {
+                food = new TomatoScrambledEggs();
+            }
+            // 我又想吃土豆肉丝, 这个还是得自己做
+            // 我觉得自己做好累哦，如果能有人帮我做就好了？
+            else if (type.Equals("土豆肉丝"))
+            {
+                food = new ShreddedPorkWithPotatoes();
+            }
+            return food;
+        }
+
+        static void Main(string[] args)
+        {
+            #region 1、正常创建类的对象
+
+            // 做西红柿炒蛋
+            Food food1 = Cook("西红柿炒蛋");
+            food1.Print();
+
+            Food food2 = Cook("土豆肉丝");
+            food2.Print();
+
+            #endregion
+
+            #region 2、简单工厂模式创建类
+
+            // 客户想点一个西红柿炒蛋        
+            Food food3 = FoodSimpleFactory.CreateFood("西红柿炒蛋");
+            food1.Print();
+
+            // 客户想点一个土豆肉丝
+            Food food4 = FoodSimpleFactory.CreateFood("土豆肉丝");
+            food2.Print();
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+
+    /// <summary>
+    /// 菜抽象类
+    /// </summary>
+    public abstract class Food
+    {
+        // 输出点了什么菜
+        public abstract void Print();
+    }
+
+    /// <summary>
+    /// 西红柿炒鸡蛋这道菜
+    /// </summary>
+    public class TomatoScrambledEggs : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份西红柿炒蛋！");
+        }
+    }
+
+    /// <summary>
+    /// 土豆肉丝这道菜
+    /// </summary>
+    public class ShreddedPorkWithPotatoes : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份土豆肉丝");
+        }
+    }
+
+    /// <summary>
+    /// 简单工厂类, 负责 炒菜
+    /// </summary>
+    public class FoodSimpleFactory
+    {
+        public static Food CreateFood(string type)
+        {
+            Food food = null;
+            if (type.Equals("土豆肉丝"))
+            {
+                food = new ShreddedPorkWithPotatoes();
+            }
+            else if (type.Equals("西红柿炒蛋"))
+            {
+                food = new TomatoScrambledEggs();
+            }
+
+            return food;
+        }
+    }
+}
