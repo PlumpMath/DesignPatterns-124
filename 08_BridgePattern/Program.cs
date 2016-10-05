e1f4145782c1d582657c90739663778e87df9559
高东霞 <dongxia.gao@dazong.com>
2016-10-05 15:31:24 +0800

    更新: 桥接模式

---
+++ 08_BridgePattern/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_BridgePattern
+{
+    class Program
+    {
+        /// <summary>
+        /// 以电视机遥控器的例子来演示桥接模式
+        /// </summary>
+        static void Main(string[] args)
+        {
+            /*
+             * 本例关于桥接模式的实现中，
+             * 遥控器的功能实现方法不在遥控器抽象类中去实现了，
+             * 而是把实现部分用来另一个电视机类去封装它，
+             * 然而遥控器中只包含电视机类的一个引用，
+             * 同时这样的设计也非常符合现实生活中的情况
+             * （我认为的现实生活中遥控器的实现——遥控器中并不包含换台，
+             * 打开电视机这样的功能的实现，遥控器只是包含了电视机上这些功能的引用，
+             * 然后红外线去找到电视机上对应功能的的实现）。
+             * 通过桥接模式，我们把抽象化和实现化部分分离开了，这样就可以很好应对这两方面的变化了。
+             */
+            // 创建一个遥控器
+            RemoteControl remoteControl = new ConcreteRemote();
+            // 长虹电视机
+            remoteControl.Implementor = new ChangHong();
+            remoteControl.On();
+            remoteControl.SetChannel();
+            remoteControl.Off();
+            Console.WriteLine();
+
+            // 三星牌电视机
+            remoteControl.Implementor = new Samsung();
+            remoteControl.On();
+            remoteControl.SetChannel();
+            remoteControl.Off();
+            Console.Read();
+        }
+    }
+
+    /// <summary>
+    /// 电视机，提供抽象方法
+    /// </summary>
+    public abstract class TV
+    {
+        public abstract void On();
+        public abstract void Off();
+        public abstract void tuneChannel();
+    }
+
+    /// <summary>
+    /// 长虹牌电视机，重写基类的抽象方法
+    /// 提供具体的实现
+    /// </summary>
+    public class ChangHong : TV
+    {
+        public override void On()
+        {
+            Console.WriteLine("长虹牌电视机已经打开了");
+        }
+
+        public override void Off()
+        {
+            Console.WriteLine("长虹牌电视机已经关掉了");
+        }
+
+        public override void tuneChannel()
+        {
+            Console.WriteLine("长虹牌电视机换频道");
+        }
+    }
+
+    /// <summary>
+    /// 三星牌电视机，重写基类的抽象方法
+    /// </summary>
+    public class Samsung : TV
+    {
+        public override void On()
+        {
+            Console.WriteLine("三星牌电视机已经打开了");
+        }
+
+        public override void Off()
+        {
+            Console.WriteLine("三星牌电视机已经关掉了");
+        }
+
+        public override void tuneChannel()
+        {
+            Console.WriteLine("三星牌电视机换频道");
+        }
+    }
+
+    /// <summary>
+    /// 抽象概念中的遥控器，扮演抽象化角色
+    /// </summary>
+    public class RemoteControl
+    {
+        // 字段
+        private TV implementor;
+
+        // 属性
+        public TV Implementor
+        {
+            get { return implementor; }
+            set { implementor = value; }
+        }
+
+        /// <summary>
+        /// 开电视机，这里抽象类中不再提供实现了，而是调用实现类中的实现
+        /// </summary>
+        public virtual void On()
+        {
+            implementor.On();
+        }
+
+        /// <summary>
+        /// 关电视机
+        /// </summary>
+        public virtual void Off()
+        {
+            implementor.Off();
+        }
+
+        /// <summary>
+        /// 换频道
+        /// </summary>
+        public virtual void SetChannel()
+        {
+            implementor.tuneChannel();
+        }
+    }
+
+    /// <summary>
+    /// 具体遥控器
+    /// </summary>
+    public class ConcreteRemote : RemoteControl
+    {
+        public override void SetChannel()
+        {
+            Console.WriteLine("---------------------");
+            base.SetChannel();
+            Console.WriteLine("---------------------");
+        }
+    }
+}
