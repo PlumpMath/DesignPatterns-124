c74d3b8b09277d80ce60d47399624782bd453fe5
高东霞 <dongxia.gao@dazong.com>
2016-10-08 12:06:14 +0800

    更新: 命令模式

---
+++ 15_CommandPattern/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_CommandPattern
+{
+    //军训场景中，具体的命令即是学生跑1000米，这里学生是命令的接收者，教官是命令的请求者，院领导是命令的发出者，即客户端角色。
+    //具体命令就是跑1000米，这自然属于学生的责任，所以是具体命令角色的成员方法，而抽象命令类定义这个命令的抽象接口。
+
+
+
+
+    // 教官，负责调用命令对象执行请求
+    public class Invoke
+    {
+        public Command _command;
+
+        public Invoke(Command command)
+        {
+            this._command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Action();
+        }
+    }
+
+    // 命令抽象类
+    public abstract class Command
+    {
+        // 命令应该知道接收者是谁，所以有Receiver这个成员变量
+        protected Receiver _receiver;
+
+        public Command(Receiver receiver)
+        {
+            this._receiver = receiver;
+        }
+
+        // 命令执行方法
+        public abstract void Action();
+    }
+
+    // 
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver)
+            : base(receiver)
+        {
+        }
+
+        public override void Action()
+        {
+            // 调用接收的方法，因为执行命令的是学生
+            _receiver.Run1000Meters();
+        }
+    }
+
+    // 命令接收者——学生
+    public class Receiver
+    {
+        public void Run1000Meters()
+        {
+            Console.WriteLine("跑1000米");
+        }
+    }
+
+    // 院领导
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 初始化Receiver、Invoke和Command
+            Receiver r = new Receiver();
+            Command c = new ConcreteCommand(r);
+            Invoke i = new Invoke(c);
+
+            // 院领导发出命令
+            i.ExecuteCommand();
+
+            Console.Read();
+        }
+    }
+}
